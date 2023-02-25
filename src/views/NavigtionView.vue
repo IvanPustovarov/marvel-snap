6af3faf28abae1e901e86a1e8b646f6f9d05048d
John <silverflash150@gmail.com>
2023-02-25 16:43:01 +0300

    add css to nav panel

--- src/views/NavigtionView.vue
+++ src/views/NavigtionView.vue
@@ -1,11 +1,110 @@
 <script setup lang="ts">
+import { RouterLink } from "vue-router"
 </script>
 
 <template>
-    <div>
-        Navifation View
-    </div>
+   <div class="nav-panel">
+    <nav>
+      <div class="logo-img">
+        <RouterLink to="/">
+          <img src="@/assets/img/logo.png" alt="" />
+        </RouterLink>
+      </div>
+      <div class="links">
+        <div class="item">
+          <RouterLink to="/">Главная</RouterLink>
+        </div>
+        <div class="item">
+          <RouterLink to="/cards">Карты</RouterLink>
+        </div>
+        <div class="item">
+          <RouterLink to="/locations">Локации</RouterLink>
+        </div>
+      </div>
+      <SearchComponent />
+    </nav>
+    <div class="br"></div>
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.nav-panel {
+  width: 100%;
+  min-height: 5rem;
+  nav {
+    background-color: rgb(27, 30, 41);
+    display: flex;
+    flex-direction: row;
+    position: relative;
+    a {
+      font-style: italic;
+      font-weight: 600;
+      color: #ffffff;
+
+      &.router-link-exact-active {
+        color: #ffffff;
+      }
+    }
+    .logo-img {
+      position: absolute;
+      cursor: pointer;
+      top: 5px;
+      left: 14px;
+      height: 5rem;
+      img {
+        width: 80%;
+        &:hover {
+          transform: perspective(1000px) rotateX(0deg) rotateY(0deg)
+            scale3d(1.035, 1.035, 1.035);
+          transition: all 0.5s ease-out;
+        }
+      }
+    }
+    .links {
+      display: flex;
+      padding: 0 0 0 10rem;
+      align-items: center;
+      .item {
+        display: flex;
+        padding: 0 0 0 0.3rem;
+        height: 100%;
+        height: 5rem;
+        align-items: center;
+        font-size: 18px;
+        a {
+          text-decoration: none;
+          padding: 2.5rem 1rem 2.5rem 0;
+        }
+        &:hover {
+          background: linear-gradient(
+                        180deg,
+                        rgba(128, 70, 222, 0.9),
+                        rgb(35, 14, 83)
+                    );
+          background-size: 200% 200%;
+        }
+      }
+    }
+  }
+  .br {
+  height: 0.7rem;
+  background: linear-gradient(-45deg, #ed193b, #a98055, #f286e2, #681d7a);
+  background-size: 400% 400%;
+  animation: gradient 10s ease infinite;
+}
+}
+
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
 </style>
